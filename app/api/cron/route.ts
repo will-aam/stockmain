c3540671c122e2ca613b311ec690b54487a03988
Will <will00987654@gmail.com>
2025-10-04 12:46:49 -0300

    Estado de 24h adicionado

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,21 +3,25 @@ import { prisma } from "@/lib/prisma";
 import { headers } from "next/headers";
 
 export async function GET(request: Request) {
-  // Pega o header 'authorization' da requisição
   const headersList = headers();
   const authorization = headersList.get("authorization");
 
-  // Verifica se a chave secreta enviada é a mesma do nosso .env
   if (authorization !== `Bearer ${process.env.CRON_SECRET}`) {
-    // Se não for, retorna um erro de não autorizado
     return new NextResponse("Unauthorized", { status: 401 });
   }
 
-  // Se a chave estiver correta, continua com a lógica de limpeza
   try {
     const twentyFourHoursAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
 
-    const result = await prisma.codigoBarras.deleteMany({
+    const productResult = await prisma.produto.deleteMany({
+      where: {
+        created_at: {
+          lt: twentyFourHoursAgo, // lt = less than (menor que)
+        },
+      },
+    });
+
+    const barcodeResult = await prisma.codigoBarras.deleteMany({
       where: {
         created_at: {
           lt: twentyFourHoursAgo,
@@ -26,12 +30,13 @@ export async function GET(request: Request) {
     });
 
     console.log(
-      `CRON JOB: ${result.count} códigos de barras antigos foram excluídos.`
+      `CRON JOB: ${productResult.count} produtos e ${barcodeResult.count} códigos de barras antigos foram excluídos.`
     );
 
     return NextResponse.json({
-      message: "Limpeza de códigos de barras antigos concluída.",
-      deletedCount: result.count,
+      message: "Limpeza de catálogos antigos concluída.",
+      deletedProducts: productResult.count,
+      deletedBarcodes: barcodeResult.count,
     });
   } catch (error) {
     console.error("Erro no CRON JOB de limpeza:", error);
